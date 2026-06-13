da582a1b3f318bc6376c4a5a5a1c1917a46d4ea3
pramod <pramodocp@gmail.com>
2026-06-13 17:46:23 +0530

    File

---
+++ student_file_manager.py
@@ -0,0 +1,14 @@
+file = open("student_log.txt", "a")
+
+student_name = input("Enter Studdent Name   :")
+
+file.write(student_name + "\n")
+
+print("Student Saved Successfully!")
+file.close()
+
+file = open("student_log.txt", "r");
+
+print(file.read());
+
+file.close();
