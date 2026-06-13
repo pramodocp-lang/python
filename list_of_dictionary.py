fb544e4c73ae6cd0d2954220a772fe6dd2479dcc
pramod <pramodocp@gmail.com>
2026-06-13 16:49:15 +0530

    List of dictionary working fine ...

---
+++ list_of_dictionary.py
@@ -0,0 +1,20 @@
+employees = [
+
+    {
+    "name": "Ramesh",
+    "department": "IT",
+    "salary": 56000
+    },
+    {
+    "name": "Mohan",
+    "department": "IT",
+    "salary": 66000
+    },
+
+]
+
+
+for employee in employees:
+    print("Name:", employee["name"])
+    print("Department:", employee["department"])
+    print("Salary:", employee["salary"])
