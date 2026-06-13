9a7eaac1a3fcff175a369015a3b23104feb803dc
pramod <pramodocp@gmail.com>
2026-06-13 16:36:03 +0530

    Employee Dictionary...

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Pramod",
+    "age": 35,
+    "city": "Delhi"
+}
+
+print(student)
+
+print(student["name"])
+print(student["city"])
---
+++ employee.py
@@ -0,0 +1,9 @@
+employee = {
+    "name": "Ramesh",
+    "department": "IT",
+    "salary": 56000
+}
+
+print("Name:", employee["name"])
+print("Department:", employee["department"])
+print("Salary:", employee["salary"])
