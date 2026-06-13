6ca6f5da06d40c4b09ae4cd992cbe8a3ca4dd431
pramod <pramodocp@gmail.com>
2026-06-13 15:56:42 +0530

    Employee Salary calculation done correctly...

---
+++ employee_salary_calculator.py
@@ -0,0 +1,14 @@
+name = input("Enter Name of the Employee:")
+
+salary = int(input("Enter Salary:"))
+
+hra = round(salary * 30/ 100, 2)
+
+bonus = round(salary * 5/ 100 , 2)
+
+print("\nEmployee Name  :",  name)
+print("\nBasic Salary   :",  salary)
+print("\nHRA            :",  hra)
+print("\nBonus          :",  bonus)
+print("\nGross Salary   :",  round(salary + hra + bonus, 2))
+
